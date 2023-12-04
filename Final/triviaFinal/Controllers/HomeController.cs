1f21260f67d681f6b0b7d22ce7ed009ae2df81fc
alowe4 <117127121+alowe4@users.noreply.github.com>
2023-12-04 15:11:36 -0800

    still doesn't work

--- Final/triviaFinal/Controllers/HomeController.cs
+++ Final/triviaFinal/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using triviaFinal.Models;
+using System.Threading.Tasks;
+using triviaFinal.Services;
+using System.Text.Json;
+
 
 namespace triviaFinal.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly QueueService _queueService;
+
+        public HomeController(QueueService queueService)
+        {
+            _queueService = queueService;
+        }
 
         public IActionResult Index()
         {
@@ -21,6 +31,29 @@ namespace triviaFinal.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> SendMessage()
+        {
+            var message = new { Content = "Hello, Azure Queue!" };
+            await _queueService.SendMessageAsync(message);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> ReceiveMessage()
+        {
+            var queueMessage = await _queueService.ReceiveMessageAsync();
+
+            if (queueMessage != null)
+            {
+                // Process the message as needed
+                var content = JsonSerializer.Deserialize<string>(queueMessage.MessageText);
+
+                // Delete the message after processing
+                await _queueService.DeleteMessageAsync(queueMessage.MessageId, queueMessage.PopReceipt);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
